53f529a4b78f2f89506161aab9291e2d4771aa8a
Omkar-Savoikar <omkarsavoikar@gmail.com>
2024-11-12 16:12:57 +0530

    Update images

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,11 +1,17 @@
 import DELETE from "./delete.png";
+import HIDDEN from "./hidden.png";
 import LIKE from "./like.png";
+import LIKED from "./liked.png";
 import MEMORY from "./memory.png";
+import VISIBLE from "./visible.png";
 
 const IMAGES = {
 	DELETE,
+	HIDDEN,
 	LIKE,
+	LIKED,
 	MEMORY,
+	VISIBLE,
 };
 
 export default IMAGES;
